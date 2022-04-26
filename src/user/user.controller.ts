00259b992411dd4d8451ad6091d44059925bb1d1
brianmuturi2 <brianmuturi2@gmail.com>
2022-04-26 10:55:56 +0300

    feat(user): implemented generating jwt token on create user

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -2,14 +2,16 @@ import { Body, Controller, Post } from '@nestjs/common';
 import { UserService } from './user.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { User } from './entities/user.entity';
+import { UserResponseInterface } from './types/userResponse.interface';
 
 @Controller('users')
 export class UserController {
   constructor(private readonly userService: UserService) {
   }
   @Post()
-  async createUser(@Body('user') createUserDto: CreateUserDto): Promise<User> {
+  async createUser(@Body('user') createUserDto: CreateUserDto): Promise<UserResponseInterface> {
     console.log('create user ', createUserDto);
-    return this.userService.createUser(createUserDto);
+    const user = await this.userService.createUser(createUserDto);
+    return this.userService.buildUserResponse(user);
   }
 }
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -3,6 +3,9 @@ import { CreateUserDto } from './dto/create-user.dto';
 import { InjectRepository } from '@nestjs/typeorm';
 import { User } from './entities/user.entity';
 import { Repository } from 'typeorm';
+import { sign } from 'jsonwebtoken';
+import { JWT_SECRET } from '../config';
+import { UserResponseInterface } from './types/userResponse.interface';
 
 @Injectable()
 export class UserService {
@@ -11,9 +14,26 @@ export class UserService {
     private readonly userRepository: Repository<User>
   ) {
   }
-  async createUser(createUserDto: CreateUserDto): Promise<User> {
+  async createUser(createUserDto: CreateUserDto): Promise<User > {
     const newUser = new User();
     Object.assign(newUser, createUserDto);
     return await this.userRepository.save(newUser);
+  }
+
+  buildUserResponse(user: User): UserResponseInterface {
+    return {
+      user: {
+        ...user,
+        token: this.generateJwt(user)
+      }
+    }
+  }
+
+  generateJwt(user: User): string {
+    return sign({
+      id: user.id,
+      username: user.username,
+      email: user.email
+    }, JWT_SECRET);
   }
 }
